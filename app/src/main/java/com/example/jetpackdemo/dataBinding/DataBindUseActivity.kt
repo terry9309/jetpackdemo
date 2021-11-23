8573457684e3455e8951acc66be1a06ba0662ba9
terry <fangxianchao@zhehekeji.com>
2021-11-23 15:19:31 +0800

    新增配置文件

--- app/src/main/java/com/example/jetpackdemo/dataBinding/DataBindUseActivity.kt
+++ app/src/main/java/com/example/jetpackdemo/dataBinding/DataBindUseActivity.kt
@@ -16,6 +16,8 @@ class DataBindUseActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<com.example.jetpackdemo.databinding.ActivityDataBindUseBinding>(this,R.layout.activity_data_bind_use)
 
+
+
         mStudent.nameF.set("terry")
         mStudent.ageF.set(12)
         binding.student = mStudent
